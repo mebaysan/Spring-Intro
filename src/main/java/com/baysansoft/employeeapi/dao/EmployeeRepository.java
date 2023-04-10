64d55f089eb7f69fb4786ba21bd5bca8eabb6bfb
mebaysan <menesbaysan@gmail.com>
2023-04-10 16:34:17 +0300

    Spring Data Rest
    We can easily expose our JPA Repositories to the web

--- src/main/java/com/baysansoft/employeeapi/dao/EmployeeRepository.java
+++ src/main/java/com/baysansoft/employeeapi/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.baysansoft.employeeapi.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.baysansoft.employeeapi.entities.Employee;
 
+// @RepositoryRestResource(path = "members") // this is optional for rest api path, default "employees"
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    
+
 }
